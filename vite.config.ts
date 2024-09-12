4f6283de4b8e901b900dd1d76eceb026be0edf6d
oxlzlo <yeojin58@naver.com>
2024-09-12 17:04:21 +0900

    Env: 배포 3트

--- vite.config.ts
+++ vite.config.ts
@@ -12,15 +12,27 @@
 //   },
 // });
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   root: '.', // 프로젝트 루트 디렉토리
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist', // 빌드 파일들이 저장될 폴더
+//     emptyOutDir: true,
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  root: '.', // 프로젝트 루트 디렉토리
   plugins: [react()],
+  publicDir: 'public', // public 폴더 설정 확인
   build: {
-    outDir: 'dist', // 빌드 파일들이 저장될 폴더
+    outDir: 'dist',
     emptyOutDir: true,
   },
 });
