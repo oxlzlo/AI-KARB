924049ea59f3a3f994ae7bba82b735f16bb52734
oxlzlo <yeojin58@naver.com>
2024-09-12 16:34:22 +0900

    Env: Config 파일 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ['@nivo/line'], // 여기 추가
+//   },
+//   build: {
+//     outDir: 'dist',
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: '.', // 프로젝트 루트 디렉토리
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@nivo/line'], // 여기 추가
-  },
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // 빌드 파일들이 저장될 폴더
+    emptyOutDir: true,
   },
 });
